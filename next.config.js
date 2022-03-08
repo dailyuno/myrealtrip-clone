76b4da4bc004ad03f6bbd88107e6ab912343b745
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-08 17:16:15 +0900

    chore: 이미지 허용 도메인 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["d2ur7st6jjikze.cloudfront.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
