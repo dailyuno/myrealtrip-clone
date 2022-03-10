ee81f75c31d08f732bc1b895fa85365b2a17eea5
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-10 16:01:23 +0900

    test: fetcher 테스트 케이스 작성

---
+++ utils/fetcher.test.ts
@@ -0,0 +1,17 @@
+import fetcher from "./fetcher";
+
+describe("fetcher", () => {
+  it("fetcher 성공", async () => {
+    const id = "20220310";
+    const data = await fetcher(`/api/fetcher/${id}`);
+    expect(data).toEqual([{ id }]);
+  });
+
+  it("fetcher 실패", async () => {
+    try {
+      await fetcher("/api/fetcher");
+    } catch (e: any) {
+      expect(e.response.status).toBe(404);
+    }
+  });
+});
