5ba0bfe81b352b88f9183d449e4cd51b4ce665da
dailyuno <34119537+dailyuno@users.noreply.github.com>
2022-03-11 16:46:36 +0900

    feat: 슬라이드 기능 추가

--- modules/offers/components/OfferCardContainer.tsx
+++ modules/offers/components/OfferCardContainer.tsx
@@ -1,5 +1,14 @@
+import { useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
 import { OfferContainer } from "../types";
 import OfferCard from "./OfferCard";
+import SwiperControl from "~/components/swiper/SwiperControl";
+import Maybe from "~/components/common/Maybe";
+
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
 
 interface OfferContainerProps {
   container: OfferContainer;
@@ -9,24 +18,33 @@ const OfferCardContainer: React.FC<OfferContainerProps> = ({
   container,
 }: OfferContainerProps) => {
   const { title, offers } = container;
+  const [swiper, setSwiper] = useState<SwiperCore | null>(null);
 
   return (
-    <div className="mb-10">
+    <div className="relative mb-10">
       <h2
         className="text-2xl mb-4 font-medium"
         data-testid="offer-container-title"
       >
         {title}
       </h2>
-      <div className="flex flex-wrap">
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={20}
+        slidesPerGroup={4}
+        onBeforeInit={(swiper) => setSwiper(swiper)}
+      >
         {offers.map((offer) => {
           return (
-            <div key={offer.id} className="w-[250px] mr-4 mb-4">
+            <SwiperSlide key={offer.id}>
               <OfferCard offer={offer} />
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
+      <Maybe test={!!swiper}>
+        <SwiperControl swiper={swiper as SwiperCore} />
+      </Maybe>
     </div>
   );
 };
